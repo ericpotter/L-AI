c487e0015756110428290fe37283971c9f1250ce
賴宗岳 <26855591lai@gmail.com>
2024-05-07 00:06:07 +0800

    2024/5/7

--- my-first-bot/bot.py
+++ my-first-bot/bot.py
@@ -1,19 +1,20 @@
 import os
-import openai
 from dotenv import load_dotenv, find_dotenv
+from openai import OpenAI
+
+
 _ = load_dotenv(find_dotenv()) # read local .env file
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
 
-
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  max_tokens=128,
-  temperature=0.5,
-  messages=[
-        {"role": "user", "content": "我叫做 oxxo"},
-        {"role": "assistant", "content": "原來你是 oxxo 呀"},
-        {"role": "user", "content": "請問我叫什麼名字？"}
-    ]
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
 )
-print(response.choices[0].message.content)
+
+print(chat_completion.choices[0].message)
