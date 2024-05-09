fbe12e93ac85db282e4aa1f3a2ab2dd72e186bc3
賴宗岳 <26855591lai@gmail.com>
2024-05-09 18:00:05 +0800

    2024//5/9
    finish the bot but without the details

--- chat bot/line bot.py
+++ chat bot/line bot.py
@@ -2,34 +2,81 @@ from flask import Flask, request
 
 # 載入 json 標準函式庫，處理回傳的資料格式
 import json
-
+import os
 # 載入 LINE Message API 相關函式庫
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import load_dotenv, find_dotenv
+# Gemini
+import pathlib
+import textwrap
+import google.generativeai as genai
+from IPython.display import display
+from IPython.display import Markdown
+# Whisper
+import whisper
+
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"),)
+
+
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+def gemini_ai(text):
+    model = genai.GenerativeModel('gemini-pro')
+    chat = model.start_chat(history=[])
+    response = chat.send_message(text)
+    return response.text
+
+def whisper_ai(fileName):
+    model = whisper.load_model("base")
+
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(fileName)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+    # print the recognized text
+    return(result.text)
 
 app = Flask(__name__)
-
 @app.route("/", methods=['POST'])
+
 def linebot():
     body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
         json_data = json.loads(body)                         # json 格式化訊息內容
-        access_token = 'bkKXd2aEQV9iCSQhDtiJ7Veupu43PrAPXT/mVKjK1tWqVln7040zDA1Z9Z/uob4TLo2xHzBYnnPvwRh5Y1PGlUJqCpzO8Z5o/EEpXrZKL8EIyxFzJrAsWdPZJMm2wI8pZzcCrI/NKea+8bvXb3ZbRwdB04t89/1O/w1cDnyilFU='
-        secret = 'fa78c5a606c071de3d33d498e2f2649e'
-        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
-        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        line_bot_api = LineBotApi(os.environ.get("LINE_BOT_TOKEN")) # 確認 token 是否正確
+        handler = WebhookHandler(os.environ.get("LINE_BOT_SECRET")) # 確認 secret 是否正確
         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
         handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
         tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
         type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
         if type=='text':
             msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
-            print(msg)                                       # 印出內容
-            reply = msg
+            reply = gemini_ai(msg)
+        elif type=='audio':
+            msgID = json_data['events'][0]['message']['id']
+            message_content = line_bot_api.get_message_content(msgID)
+            with open(f'temp.m4a', 'wb') as fd:
+                fd.write(message_content.content)
+            msg = whisper_ai('temp.m4a')
+            reply = gemini_ai(msg)
         else:
             reply = '你傳的不是文字呦～'
-        print(reply)
+            
+        
         line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
     except:
         print(body)                                          # 如果發生錯誤，印出收到的內容
@@ -37,3 +84,6 @@ def linebot():
 
 if __name__ == "__main__":
     app.run()
+
+
+#ngrok http 127.0.0.1:5000
