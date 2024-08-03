745db16c1c8d20477be4a53e6b84181aa7264610
賴宗岳 <26855591lai@gmail.com>
2024-08-03 22:40:13 +0800

    Update main bot.py

--- langchain/main bot.py
+++ langchain/main bot.py
@@ -32,22 +32,22 @@ def is_traditional_chinese_or_digit(text):
     return all('\u4e00' <= char <= '\u9fff' or char.isdigit() for char in text), converted_text
 
 # 打印狀態的實用函數
-def RPrint(preface="狀態: "):
-    def print_and_return(x, preface=""):
+def RPrint(preface = "狀態: "):
+    def print_and_return(x, preface = ""):
         print(f"{preface}{x}")
         return x
-    return RunnableLambda(partial(print_and_return, preface=preface))
+    return RunnableLambda(partial(print_and_return, preface = preface))
 
 # 美化打印狀態的實用函數
-def PPrint(preface="狀態: "):
-    def print_and_return(x, preface=""):
+def PPrint(preface = "狀態: "):
+    def print_and_return(x, preface = ""):
         pprint(preface, x)
         return x
-    return RunnableLambda(partial(print_and_return, preface=preface))
+    return RunnableLambda(partial(print_and_return, preface = preface))
 
 # 使用 Pydantic 模型和 LLM 提取信息的函數
 def RExtract(pydantic_class, llm, prompt):
-    parser = PydanticOutputParser(pydantic_object=pydantic_class)
+    parser = PydanticOutputParser(pydantic_object = pydantic_class)
     instruct_merge = RunnableAssign({'format_instructions': lambda x: parser.get_format_instructions()})
     
     def preparse(string):
@@ -60,42 +60,48 @@ def RExtract(pydantic_class, llm, prompt):
 
 # 個人信息的資料庫模型
 class PersonalInfoBase(BaseModel):
-    height: float = Field(0, description="聊天用戶的身高（厘米），如果未知則為 '0'")
-    weight: float = Field(0, description="聊天用戶的體重（公斤），如果未知則為 '0'")
-    gender: str = Field('unknown', description="聊天用戶的性別，如果未知則為 'unknown'")
-    period: str = Field('unknown', description="聊天用戶的月經問題摘要，如果未知則為 'unknown'，如果用戶為男性則為 None")
-    menopause: str = Field('unknown', description="聊天用戶是否絕經，如果未知則為 'unknown'，如果用戶為男性則為 None")
-    hand_foot: str = Field('unknown', description="聊天用戶的手腳是否經常感到寒冷，如果未知則為 'unknown'")
-    body: str = Field('unknown', description="聊天用戶的身體是否經常感到熱或出汗，如果未知則為 'unknown'")
-    defecation: str = Field('unknown', description="聊天用戶的排便狀況摘要，如果正常則為 'normal'")
-    digestive_system: str = Field('unknown', description="聊天用戶的消化系統狀況摘要，如果排便正常則為 None")
-    meals: str = Field('unknown', description="聊天用戶的膳食是否正常，如果正常則為 'normal'")
-    mouth: str = Field('unknown', description="聊天用戶的口腔狀況摘要，包括苦口、口乾、口臭和口渴，如果正常則為 'normal'")
-    urine: str = Field('unknown', description="聊天用戶的尿液狀況摘要，如果用戶沒有口渴則為 'normal'")
-    sleep: str = Field('unknown', description="聊天用戶的睡眠狀況摘要，包括睡眠時間、淺睡問題、無法入睡和早醒，如果正常則為 'normal'")
-    phone: str = Field('unknown', description="聊天用戶是否在睡前使用手機，如果睡眠正常則為 None")
-    stay_up: str = Field('unknown', description="聊天用戶是否經常熬夜")
-    pressure: str = Field('unknown', description="聊天用戶是否承受某些壓力及其原因，如果正常則為 'normal'")
-    open_problems: str = Field("", description="未知的信息")
-    current_goals: str = Field("", description="代理要處理的當前目標")
-    summary: str = Field("", description="對話的運行摘要。用新輸入更新這個摘要")
+    height: float = Field(0, description = "聊天用戶的身高（公分），如果未知則為 '0'")
+    weight: float = Field(0, description = "聊天用戶的體重（公斤），如果未知則為 '0'")
+    gender: str = Field('unknown', description = "聊天用戶的性別，如果未知則為 'unknown'")
+    period: str = Field('unknown', description = "聊天用戶的月經問題摘要，如果未知則為 'unknown'，如果用戶為男性則為 'none'")
+    menopause: str = Field('unknown', description = "聊天用戶是否絕經，如果未知則為 'unknown'，如果用戶為男性則為 'none'")
+    hand_foot: str = Field('unknown', description = "聊天用戶的手腳是否經常感到寒冷，如果正常則為 'normal'")
+    body: str = Field('unknown', description = "聊天用戶的身體是否經常感到熱或出汗，如果正常則為 'normal'")
+    defecation: str = Field('unknown', description = "聊天用戶的排便狀況摘要，如果整體正常則為 'normal'")
+    digestive_system: str = Field('unknown', description = "聊天用戶的消化系統狀況摘要，如果排便正常則為 'none'")
+    meals: str = Field('unknown', description = "聊天用戶的三餐是否正常，如果正常則為 'normal'")
+    mouth: str = Field('unknown', description = "聊天用戶的口腔狀況摘要，一定包含是否口苦、口乾、口臭和口渴，如果正常則為 'normal'")
+    urine: str = Field('unknown', description = "聊天用戶的尿液狀況摘要，如果用戶不會口渴則為 'normal'")
+    sleep: str = Field('unknown', description = "聊天用戶的睡眠狀況摘要，一定包括睡眠時間、淺睡問題、無法入睡和早醒，如果正常則為 'normal'")
+    phone: str = Field('unknown', description = "聊天用戶是否在睡前使用手機，如果睡眠正常則為 'none'")
+    stay_up: str = Field('unknown', description = "聊天用戶是否經常熬夜")
+    pressure: str = Field('unknown', description = "聊天用戶是否承受某些壓力及其原因，如果沒有則為 'none'")
+    open_problems: str = Field("", description = "資訊為'unknown'並需要詢問的")
+    current_goals: str = Field("", description = "Current goal for the agent to address")
+    summary: str = Field("", description ="目前到現在對話的摘要。用新輸入更新這個摘要")
 
 # 聊天機器人的提示模板
 main_bot_prompt = ChatPromptTemplate.from_messages([
-    ("system", "您好，我是AI中醫諮詢系統L AI，請問有什麼需要幫忙的嗎？"),
+    ("system", "您好，我是AI中醫諮詢系統L AI，請問今天需要諮詢嗎？"),
     ("assistant", "{output}"),
     ("user", "{input}"),
 ])
 
 # 解析器提示模板
 parser_prompt = ChatPromptTemplate.from_template(
-    "您是聊天助手，並試圖跟蹤有關對話的信息。"
+    "您是聊天助手，並試圖追蹤有關對話的信息。"
     "您剛剛收到了用戶的消息。請根據聊天內容填寫以下架構。"
     "\n\n{format_instructions}"
     "\n\n舊的知識庫: {info_base}"
     "\n\n助手回應: {output}"
     "\n\n用戶消息: {input}"
     "\n\n新的知識庫: "
+)
+
+# 確認資訊完整
+fail_info = (
+    "你沒辦法給我適當的診斷直到搜集使用者的全部資訊"
+    "包含以下部分：{open_problems}"
 )
 
 # 加載環境變量
@@ -106,7 +112,7 @@ if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("請提供您的 Google API Key")
 
 # 初始化語言模型
-model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash")
 instruct_llm = model | StrOutputParser()
 chat_llm = model | StrOutputParser()
 external_chain = main_bot_prompt | chat_llm
@@ -124,7 +130,7 @@ internal_chain = (
 state = {'info_base': PersonalInfoBase()}
 
 # 聊天生成函數
-def chat_gen(message, history=[], return_buffer=True):
+def chat_gen(message, history = [], return_buffer = True):
     global state
     state['input'] = message
     state['history'] = history
@@ -151,7 +157,7 @@ def chat_gen(message, history=[], return_buffer=True):
         yield buffer if return_buffer else token_output
 
 # 初始聊天歷史
-chat_history = [[None, "您好，我是AI中醫諮詢系統L AI，請問有什麼需要協助的嗎？"]]
+chat_history = [[None, "您好，我是AI中醫諮詢系統L AI，請問今天有需要諮詢嗎？"]]
 
 # Gradio 聊天函數
 def gradio_chat(input, history):
