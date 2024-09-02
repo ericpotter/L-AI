75d1600304483b8f6fb714f165c9418ed82f561f
賴宗岳 <26855591lai@gmail.com>
2024-09-02 09:20:56 +0800

    adding new prompt and chain

--- chain.py
+++ chain.py
@@ -14,6 +14,16 @@ database_getter = RunnableLambda(itemgetter('info_base')) | data.updateData | da
 # external chain status
 external_chain = prompt.chat_prompt | model.chat_llm
 
+self_description_chain = prompt.self_description_prompt | model.chat_llm
+basic_info_chain = prompt.basic_info_prompt | model.chat_llm
+sleep_chain = prompt.sleeping_prompt | model.chat_llm
+mouth_chain = prompt.mouth_prompt | model.chat_llm
+bowel_chain = prompt.bowel_habit_prompt | model.chat_llm
+period_chain = prompt.period_prompt | model.chat_llm
+menopause_chain = prompt.menopause_prompt | model.chat_llm
+mind_chain = prompt.mind_prompt | model.chat_llm
+
+
 check_chain = prompt.check_prompt | model.check_llm
 
 # internal chain status
